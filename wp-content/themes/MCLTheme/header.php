ab80b6a710a95f57413e4c6230f12905dcd675ab
Laurent Binder <cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh>
2021-06-30 17:35:53 +0200

    add header and footer for mytheme

--- wp-content/themes/MCLTheme/header.php
+++ wp-content/themes/MCLTheme/header.php
@@ -1,15 +1,11 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head> -->
-
-<?php get_header() ?>
+    <?php wp_head() ?>
+</head>
 <body>
-    Bonjour mon site perso
-</body>
-<?php get_footer() ?>
-<!-- </html> -->
+    
